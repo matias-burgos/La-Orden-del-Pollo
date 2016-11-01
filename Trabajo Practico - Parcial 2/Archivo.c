21500aebc7711c087904722df3eb4993ca463377
matias-burgos <matiasburgos_98@hotmail.com>
2016-11-01 15:48:41 -0300

    Parcial

---
+++ Trabajo Practico - Parcial 2/Archivo.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Archivo.h"
+#inglude "string.h"
+persona cargar_persona()
+{
+    persona aux;
+    printf("\n Ingresar nombre y apellido: ");
+    gets(aux.nombreApellido);
+    printf("\n Ingrese cantidad de articulos: ");
+    scanf("%d", aux.cantArticulos);
+    printf("\n Ingrese el tipo de persona: \n1:Embarazada \n2:Jubilado \n3:Común");
+    scanf("%d", aux.tipo_cliente);
+    printf("\n Ingresar su tipo de pago: \n1:Efectivo; \n2:Credito o Debito \n3:Todos");
+    scanf("%d", aux.tipo_pago);
+    aux.tiempoDeEspera=0;
+    aux.tiempoProcesado=0;
+    return aux;
+}
+
+int cargar_archivo(char nombre[])
+{
+    int i=0;
+    persona personas;
+    char letra='s';
+    FILE*archi=fopen(nombre, "w+b");
+    if(archi!=NULL)
+    {
+        while(i<30)
+        {
+            personas=cargar_persona();
+            fwrite(personas, sizeof(persona), 1, archi);
+            i++;
+        }
+    }
+    fclose(archi);
+    return i;
+}
+
+void mostrar_persona(persona aux)
+{
+    printf("\n Nombre y Apellido: _%s", aux.nombreApellido);
+    printf("\n Cantidad de articulos que posee: _%d", aux.cantArticulos);
+    printf("\n Tipo de pago: _%d", aux.tipo_pago);
+    printf("\n Tipo de cliente: _%d", aux.tipo_cliente);
+}
+
+void mostrar_archivo(char nombre[])
+{
+    persona aux;
+    int i=0;
+    FILE*archi=fopen(nombre, "r+b")
+    if(archi!=NULL)
+    {
+        while(fread(&aux, sizeof(persona), 1, archi)!=0)
+        {
+            i++;
+            printf("\n Persona %d", i);
+            mostrar_persona(aux);
+        }
+    }
+    fclose(archi);
+}
---
+++ Trabajo Practico - Parcial 2/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Archivo.h"
+typedef struct
+{
+      char nombreApellido [40];
+      int cantArticulos;            // es el tiempo de ejecución
+      int tiempoDeEspera;     // es el tiempo de respuesta
+      int tiempoProcesado     // es el tiempo que ya fue procesado en la línea de caja
+      int tipo_cliente;              //  prioridad (1: embarazada, 2: jubilado y 3: común)
+      int tipo_pago;                //  1 efectivo, 2 crédito o débito, 3 todos
+} persona;
+
+typedef struct
+{
+      persona p;
+      struct nodoArbol * izq;
+      struct nodoArbol * der;
+} nodoArbol;
+
+
+int main()
+{
+    char
+}
